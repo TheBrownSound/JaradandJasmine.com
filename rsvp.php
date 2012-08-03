8a0ce7767740aa8964ba64b7279ec22fe67ca4a6
Jarad Comingdeer <jarad@jcomingdeer.com>
2012-08-03 16:57:17 -0700

    stupid simple contact form

---
+++ rsvp.php
@@ -0,0 +1,13 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone']
+$message = $_POST['message'];
+$attending = $_POST['attending'];
+$formcontent="From: $email \n Names: $name \n Attending: $attending \n Message: $message";
+$recipient = "wedding@jcomingdeer.com";
+$subject = "Wedding RSVP";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
